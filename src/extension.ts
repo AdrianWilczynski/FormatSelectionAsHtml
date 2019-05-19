4b4844fc4bec693d5aae7700d24338c61665fea8
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-05-19 15:24:45 +0200

    Increase print width, use editor's settings for indentation

--- src/extension.ts
+++ src/extension.ts
@@ -31,12 +31,18 @@ async function formatSelectionAsHTML() {
   const document = vscode.window.activeTextEditor.document;
   const selection = vscode.window.activeTextEditor.selection;
 
+  const tabSize = vscode.window.activeTextEditor.options.tabSize;
+  const insertSpaces = vscode.window.activeTextEditor.options.insertSpaces;
+
   const selectedText = document.getText(selection);
 
   const formattedText = prettier
     .format(selectedText, {
       parser: "html",
-      htmlWhitespaceSensitivity: htmlWhitespaceSensitivity
+      htmlWhitespaceSensitivity: htmlWhitespaceSensitivity,
+      tabWidth: typeof tabSize === "number" ? tabSize : 4,
+      useTabs: typeof insertSpaces === "boolean" ? !insertSpaces : false,
+      printWidth: 120
     })
     .replace(/[\r\n]+$/, "");
 
