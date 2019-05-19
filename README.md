e1bc73d01161a0c785201c8f5376749eb8b528ad
AdrianWilczynski <adrian.k.wilczynski@gmail.com>
2019-05-19 11:23:44 +0200

    Done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Format Selection As HTML
+
+Format selected text as HTML using Prettier. Useful for code embedded in strings or templating languages without formatting support (like Razor).
+
+## Instruction
+
+Use `Format Selection As HTML` command from context menu or Command Palette.
+
+## Presentation
+
+![Example](img/example.gif)
+
+## Configuration
+
+Exposes Prettier's HTML Whitespace Sensitivity setting through `formatSelectionAsHtml.htmlWhitespaceSensitivity` property.
---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+import * as vscode from "vscode";
+import * as prettier from "prettier";
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(vscode.commands.registerCommand("extension.formatSelectionAsHtml", formatSelectionAsHTML));
+}
+
+export function deactivate() {}
+
+async function formatSelectionAsHTML() {
+  if (!vscode.window.activeTextEditor) {
+    return;
+  }
+
+  const htmlWhitespaceSensitivity = vscode.workspace
+    .getConfiguration()
+    .get("formatSelectionAsHtml.htmlWhitespaceSensitivity");
+
+  if (
+    !htmlWhitespaceSensitivity ||
+    typeof htmlWhitespaceSensitivity !== "string" ||
+    !(
+      htmlWhitespaceSensitivity === "css" ||
+      htmlWhitespaceSensitivity === "strict" ||
+      htmlWhitespaceSensitivity === "ignore"
+    )
+  ) {
+    return;
+  }
+
+  const document = vscode.window.activeTextEditor.document;
+  const selection = vscode.window.activeTextEditor.selection;
+
+  const selectedText = document.getText(selection);
+
+  const formattedText = prettier.format(selectedText, {
+    parser: "html",
+    htmlWhitespaceSensitivity: htmlWhitespaceSensitivity
+  });
+
+  await vscode.window.activeTextEditor.edit(builder => builder.replace(selection, formattedText));
+}
